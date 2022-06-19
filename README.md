cc8fcd9c64b69972c5f58869f029adee1e14e6c0
Juan Pablo Lassala <pablo.lassala@globallogic.com>
2022-06-19 14:00:19 -0300

    First commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Empty boilerplate for Typescript Projects
---
+++ src/index.js
@@ -0,0 +1,24 @@
+"use strict";
+exports.__esModule = true;
+var ws_1 = require("ws");
+var wsServer = new ws_1.WebSocketServer({
+    port: 8080,
+    perMessageDeflate: {
+        zlibDeflateOptions: {
+            // See zlib defaults.
+            chunkSize: 1024,
+            memLevel: 7,
+            level: 3
+        },
+        zlibInflateOptions: {
+            chunkSize: 10 * 1024
+        },
+        // Other options settable:
+        clientNoContextTakeover: true,
+        serverNoContextTakeover: true,
+        serverMaxWindowBits: 10,
+        // Below options specified as default values.
+        concurrencyLimit: 10,
+        threshold: 1024
+    }
+});
